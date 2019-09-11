a374ad94c2f0594878df07f3b458cbff1a0ef07c
ruggz13 <nicolasruggieri7@gmail.com>
2019-09-11 11:30:27 -0400

    added restaurant container to route

--- nyk/src/containers/Post_Login/RestaurantContainer.js
+++ nyk/src/containers/Post_Login/RestaurantContainer.js
@@ -11,29 +11,34 @@ class RestaurantContainer extends Component {
   }
   handleRatingChange = (event, restaurant) => {
     debugger;
-    restaurant.rating = event.target.value
-    let col = [...this.state.restaurants]
-    let i = col.indexOf(restaurant)
-    this.setState({
-      rating: [...col.slice(0, i),
-      { ...restaurant, rating: event.target.value },
-      ...col.slice(i + 1)]
-    }
-    fetch("http://localhost:3001/reviews", {
-      method: "Post"
-      headers: {
-        "Accepts": "application/json"
-      "Allows": "application/json"
-      }
-    })
-    )
-
+    restaurant.rating = event.target.value;
+    let col = [...this.state.restaurants];
+    let i = col.indexOf(restaurant);
+    this.setState(
+      {
+        rating: [
+          ...col.slice(0, i),
+          { ...restaurant, rating: event.target.value },
+          ...col.slice(i + 1)
+        ]
+      },
+      fetch("http://localhost:3001/reviews", {
+        method: "Post",
+        headers: {
+          Accepts: "application/json",
+          Allows: "application/json"
+        }
+      })
+    );
   };
   render() {
     return this.state.restaurants.map(restaurant => (
-      <Restaurant restaurant={restaurant} handleRatingChange={this.handleRatingChange} key={restaurant.id} />
-    )
-    );
+      <Restaurant
+        restaurant={restaurant}
+        handleRatingChange={this.handleRatingChange}
+        key={restaurant.id}
+      />
+    ));
   }
 }
 
