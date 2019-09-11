7aa3fd6cab985454d1ba7d323c878955e5b7477a
github username <github email address>
2019-09-10 22:08:19 -0400

    changed class to classname

--- nyk/src/components/ReviewsForm.js
+++ nyk/src/components/ReviewsForm.js
@@ -5,9 +5,9 @@ class ReviewForm extends Component {
   render() {
     return (
       <Fragment>
-        <form class="rating">
+        <form className="rating">
           <input type="radio" id="star5" name="rating" value="5" />
-          <label class="full" for="star5" title="Awesome - 5 stars"></label>
+          <label className="full" for="star5" title="Awesome - 5 stars"></label>
           <input
             type="radio"
             id="star4half"
@@ -15,21 +15,29 @@ class ReviewForm extends Component {
             value="4 and a half"
           />
           <label
-            class="half"
+            className="half"
             for="star4half"
             title="Pretty good - 4.5 stars"
           ></label>
           <input type="radio" id="star4" name="rating" value="4" />
-          <label class="full" for="star4" title="Pretty good - 4 stars"></label>
+          <label
+            className="full"
+            for="star4"
+            title="Pretty good - 4 stars"
+          ></label>
           <input
             type="radio"
             id="star3half"
             name="rating"
             value="3 and a half"
           />
-          <label class="half" for="star3half" title="Meh - 3.5 stars"></label>
+          <label
+            className="half"
+            for="star3half"
+            title="Meh - 3.5 stars"
+          ></label>
           <input type="radio" id="star3" name="rating" value="3" />
-          <label class="full" for="star3" title="Meh - 3 stars"></label>
+          <label className="full" for="star3" title="Meh - 3 stars"></label>
           <input
             type="radio"
             id="star2half"
@@ -37,28 +45,36 @@ class ReviewForm extends Component {
             value="2 and a half"
           />
           <label
-            class="half"
+            className="half"
             for="star2half"
             title="Kinda bad - 2.5 stars"
           ></label>
           <input type="radio" id="star2" name="rating" value="2" />
-          <label class="full" for="star2" title="Kinda bad - 2 stars"></label>
+          <label
+            className="full"
+            for="star2"
+            title="Kinda bad - 2 stars"
+          ></label>
           <input
             type="radio"
             id="star1half"
             name="rating"
             value="1 and a half"
           />
-          <label class="half" for="star1half" title="Meh - 1.5 stars"></label>
+          <label
+            className="half"
+            for="star1half"
+            title="Meh - 1.5 stars"
+          ></label>
           <input type="radio" id="star1" name="rating" value="1" />
           <label
-            class="full"
+            className="full"
             for="star1"
             title="Sucks big time - 1 star"
           ></label>
           <input type="radio" id="starhalf" name="rating" value="half" />
           <label
-            class="half"
+            className="half"
             for="starhalf"
             title="Sucks big time - 0.5 stars"
           ></label>
